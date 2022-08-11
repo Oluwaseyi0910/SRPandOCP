2b9522e566062b818b01acf943c8db6e6db5db03
Oluwaseyi0910 <110403016+Oluwaseyi0910@users.noreply.github.com>
2022-08-11 16:30:45 +0100

    Add project files.

---
+++ SRPandOCP/OpenClosedPrinciple.cs
@@ -0,0 +1,44 @@
+﻿using SRP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    /// <summary>
+    /// This namespace describes the Open/Close Principle using Abstraction
+    /// </summary>
+    public abstract class ReportGeneration
+    {
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public abstract void GenerateReport(Employee em);
+
+
+
+    }
+    /// <summary>
+    /// Class to generate Crystal report
+    /// </summary>
+    public class CrystalReportGeneration : ReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            // Generate crystal report.
+        }
+    }
+    /// <summary>
+    /// Class to generate PDF report
+    /// </summary>
+    public class PDFReportGeneration : ReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            // Generate PDF report.
+        }
+    }
+}
---
+++ SRPandOCP/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    /// <summary>
+    /// This namespace describes the Single Responsibilty Principle (SRP)
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Report Generated");
+        }
+    }
+    public class Employee
+    {
+
+        public int Employee_Id { get; set; }
+        public string Employee_Name { get; set; }
+
+        /// <summary>
+        /// This method used to insert into employee table
+        /// </summary>
+        /// <param name="em">Employee object</param>
+        /// <returns>Successfully inserted or not</returns>
+        public bool InsertIntoEmployeeTable(Employee em)
+        {
+            try
+            {
+                // to create an exception
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+
+            // Insert into employee table.
+            return true;
+        }
+
+    }
+    public class ReportGeneration
+    {
+        /// <summary>
+        /// Method to compile generated report
+        /// </summary>
+        /// <param name="em"></param>
+        public void CompileReport(List<ReportGeneration> reports)
+        {
+            // Compile all EmployeeData Reports.
+        }
+    }
+}
+
+
